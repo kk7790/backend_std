893cfc9f160b36c33f81efa0668c2ec892aee9b4
kk7790 <chorang_2307@naver.com>
2023-03-12 22:22:41 +0900

    commit ok

---
+++ first_0312/src/test.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class test {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<String> test_list = new ArrayList<>();
+
+        test_list.add("test1");
+        test_list.add("test2");
+        test_list.add("test3");
+        test_list.add("test4");
+        test_list.add("test5");
+
+        System.out.println(test_list);
+
+    }
+}
+
+class List_test{
+
+}
